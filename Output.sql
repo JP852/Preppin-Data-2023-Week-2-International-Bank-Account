baebd7ba76941870c8133099efcfafa75d0f0493
JP852 <142391590+JP852@users.noreply.github.com>
2024-04-30 09:16:40 +0100

    Update Output.sql

--- Output.sql
+++ Output.sql
@@ -1,3 +1,11 @@
+-- In the Transactions table, there is a Sort Code field which contains dashes. We need to remove these so just have a 6 digit string
+-- Use the SWIFT Bank Code lookup table to bring in additional information about the SWIFT code and Check Digits of the receiving bank account
+-- Add a field for the Country Code
+      -- Hint: all these transactions take place in the UK so the Country Code should be GB
+-- Create the IBAN as above
+      -- Hint: watch out for trying to combine string fields with numeric fields - check data types
+-- Remove unnecessary fields
+
 SELECT TRANSACTION_ID
 ,CASE WHEN (LENGTH(transaction_id)>1) THEN 'GB' ELSE 'ERROR' END
 ||
